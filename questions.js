62236370f29c4f0dcd52735d0b617718f07503b5
SyedMuhammadMurtaza <smmhj19@gmail.com>
2024-04-18 15:59:01 +0500

    First push

---
+++ questions.js
@@ -0,0 +1,32 @@
+"use strict";
+//Q2 
+let firstName = "Murtaza";
+console.log(`Hello ${firstName} would you like to learn some Python today?`);
+//Q3
+let personName = "syed murtaza";
+let titlecaseName = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+console.log("Titlecase:", titlecaseName);
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(titlecaseName);
+//Q4
+console.log("Imam Ali once said,", '"The disease of the heart is worse than the disease of the body."');
+//Q5
+let famous_person = "Imam Ali";
+let message = "The disease of the heart is worse than the disease of the body";
+console.log(`${famous_person} Once said, "${message}."`);
+//Q6
+let secondName = "\t \n Syed Murtaza \t";
+console.log("Name with white spaces:", secondName);
+console.log("Name without white spaces:", secondName.trim());
+//Q7
+let number = 4;
+console.log("Addition:", number + number);
+console.log("Subtraction:", 12 - number);
+console.log("Multiplication:", number * 2);
+console.log("Division:", 32 / number);
+//Q8
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(32 / 4);
